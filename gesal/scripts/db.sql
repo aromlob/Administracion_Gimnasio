94b86120f9dccb953c8322eb589f695f4e1dbea8
Santiago Fuentes <sfueesp619@g.educaand.es>
2024-11-08 09:23:16 +0100

    tabla cliente creada

---
+++ gesal/scripts/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `cliente` (
+    `Id` int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre` varchar(50) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `fecha_registro` date NOT NULL,
+    `numero_telefono` int(10) NOT NULL
+);
