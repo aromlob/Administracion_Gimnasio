62fbe73052694fd950bb398a52cd59f882ae6913
aromlob <aromerolob@gmail.com>
2024-11-15 17:39:54 +0100

    Cambios en el pug

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const clienteController = require('../controllers/clienteControllers');
 
 // Ruta para obtener todos los clientes y renderizar la lista
-router.get('/clientes', clienteController.cliente);
+router.get('/', clienteController.cliente);
 
 // Ruta para mostrar el formulario para agregar un nuevo cliente
-router.get('/clientes/add', clienteController.clienteAddFormulario);
+router.get('/add', clienteController.clienteAddFormulario);
 
 // Ruta para agregar un cliente a la base de datos
-router.post('/clientes/add', clienteController.clienteAdd);
+router.post('/add', clienteController.clienteAdd);
 
 // Ruta para mostrar el formulario para borrar un cliente
-router.get('/clientes/delete/:Id_cliente', clienteController.clienteDeleteFormulario);
+router.get('/delete/:Id_cliente', clienteController.clienteDeleteFormulario);
 
 // Ruta para borrar un cliente de la base de datos
-router.post('/clientes/delete/:Id_cliente', clienteController.clienteDel);
+router.post('/delete/:Id_cliente', clienteController.clienteDel);
 
 // Ruta para mostrar el formulario para editar un cliente
-router.get('/clientes/edit/:Id_cliente', clienteController.clienteEditFormulario);
+router.get('/edit/:Id_cliente', clienteController.clienteEditFormulario);
 
 // Ruta para editar un cliente en la base de datos
-router.post('/clientes/edit/:Id_cliente', clienteController.clienteEdit);
+router.post('/edit/:Id_cliente', clienteController.clienteEdit);
 
 module.exports = router;
